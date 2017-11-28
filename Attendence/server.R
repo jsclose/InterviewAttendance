2d964313107f88faef2ebcfe4e6af1eb5676243b
Peter Kaplan <pkap@umich.edu>
2017-11-28 15:42:42 -0500

    shiny

--- Attendence/server.R
+++ Attendence/server.R
@@ -2,21 +2,16 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
-int <- interviews
-int$Date <- as.Date(int$Date, format = "%Y-%m-%d")         
+    
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
+  interviews <- read.csv("data.csv")
+  
+  int <- interviews
+  int$Date <- as.Date(int$Date, format = "%Y-%m-%d")     
+  
   output$distPlot <- renderPlot({
-    
-                                      
 
-   # dat <-int%>%
-    #  select(Attendance, Date) %>%
-     # filter(Date > "2015-01-04" & Date <"2015-09-18")%>%
-      #group_by(Attendance) %>%
-      #summarise(count = n())
-    
     dat <- int%>%
       select(Attendance, Date) %>%
       filter(Date > input$daterange3[1] & Date <input$daterange3[2]) %>%
@@ -27,44 +22,42 @@ shinyServer(function(input, output) {
       geom_bar(stat="identity") + ylim(0, 1000)
 
     plot
-    # generate bins based on input$bins from ui.R
-    #x    <- interviews[, 24] 
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
     
   })
   
   output$cityPlot <- renderPlot({
-    
-    
-    
-    
-    
      dat <-int%>%
        select(Venue, Attendance) %>%
        filter(Venue == input$city)%>%
        group_by(Attendance) %>%
        summarise(count = n())
      
-    
     plot<-ggplot(data=dat, aes(x=Attendance, y=count, fill = (Attendance)))+
       geom_bar(stat="identity") + ylim(0, 1000)
     
     plot
-    # generate bins based on input$bins from ui.R
-    #x    <- interviews[, 24] 
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
     
   })
     
-  
- # output$attendence <- renderDataTable(table(interviews$Attendance))
+  output$featurePlot <- renderPlot({
+    
+    dat <- int
+    for (word in input$var){
+      dat <-int%>%
+        filter(int[, as.numeric(word)] == "yes")
+    }
+     dat <- dat %>% group_by(Attendance) %>%
+      summarise(count = n())
+     
+    percent <-dat$count[2]   / (dat$count[2]   + dat$count[1]  ) * 100 
 
+    plot<-ggplot(data=dat, aes(x=Attendance[2], y=percent))+
+      geom_bar(stat="identity", fill="#0072B2", colour="black") + ylim(0, 100)
+    
+    plot
+    
+  })
+  
   output$attendence <- renderTable(  dat <- int%>%
                                        select(Attendance, Date) %>%
                                        filter(Date > input$daterange3[1] & Date <input$daterange3[2]) %>%
--- Attendence/ui.R
+++ Attendence/ui.R
@@ -1,13 +1,20 @@
 
 library(shiny)
 
+
+interviews <- read.csv("data.csv")
+
+int <- interviews
+int$Date <- as.Date(int$Date, format = "%Y-%m-%d")     
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Interview Attendence"),
+  titlePanel("Interview Attendence For Various Clients in India"),
   
-  
+  h3("
+The data pertains to the recruitment industry in India for the years 2014-2016 and deals with candidate interview attendance for various clients. When candidates don't appear for interviews, valuable time and resources are lost. "),
   fluidRow(
     column(3,
            
@@ -37,7 +44,22 @@ shinyUI(fluidPage(
     column(4, offset = 1,
            plotOutput("cityPlot")
     )
+  ),
+  fluidRow(
+    column(3,
+           checkboxGroupInput("var", "Features to turn on",
+                              c("Candidate Permission To Start Immediately" = 13,
+                                "Hope There Are No Unscheduled Meetings" = 14,
+                                "Candidate Supplied Alternative Number" = 16,
+                                "Candidate Can be called 3 hours in advance" = 15,
+                                "Candidate Recently Updated Resume" = 17,
+                                "Candidate Confirmed Venue Location" = 18))
+    ),
+    column(4, offset = 1,
+           plotOutput("featurePlot")
+    )
   )
+  
  
   # Sidebar with a slider input for number of bins 
   
